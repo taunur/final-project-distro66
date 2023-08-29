2792ea891642117ff3ac0445e9d351d8e73d6687
Taufik Nurrahman <official.taufik980@gmail.com>
2023-08-29 22:33:47 +0700

    deploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux"; // Import Provider
+import store from "./app/redux/store"; // Import Redux store
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
